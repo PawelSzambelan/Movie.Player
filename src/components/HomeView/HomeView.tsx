413dc0c036902d7b77801115105b6f65068d2467
Pawel Szambelan <p.szambelan1@gmail.com>
2021-06-11 00:44:56 +0200

    add styling for home view

--- src/components/HomeView/HomeView.tsx
+++ src/components/HomeView/HomeView.tsx
@@ -1,163 +1,30 @@
 import React from 'react';
-import {useEffect, useState} from "react";
-import {MediaRestApi} from "../../restapi/media/MediaRestApi";
-import {MediaDto} from "../../restapi/media/MediaDto";
-import {ImageDto} from "../../restapi/media/ImageDto";
+import {MediaListView} from "../MediaListView/MediaListView";
+import {Typography} from "@material-ui/core";
+import {VerticalSpace} from "../VerticalSpace";
 import {makeStyles} from "@material-ui/core/styles";
-import {PATH_FOR_IMAGES} from "../constants/imgPath";
-import {Button, IconButton, TextField} from "@material-ui/core";
-import * as yup from "yup";
-import {useFormik} from "formik";
-import Pagination from '@material-ui/lab/Pagination';
-import PlayCircleOutlineIcon from '@material-ui/icons/PlayCircleOutline';
-import {useHistory} from "react-router-dom";
-
-type MediaToDisplay = {
-    id: number;
-    title: string;
-    image: string;
-}
 
 const useStyles = makeStyles({
     container: {
-        maxWidth: "500px",
-    },
-    imageWrapper: {
-        position: "relative",
-        paddingBottom: "56.2%",
-    },
-    image: {
-        position: "absolute",
-        objectFit: "cover",
-        width: "100%",
-        height: "100%",
-        "&:hover": {
-            opacity: "70%"
-        }
-    },
-    playButton: {
-        position: "absolute",
-        top: "30%",
-        left: "35%",
-        zIndex: 20,
-    },
-    iconSize: {
-        fontSize: "4em"
-    },
-});
-
-const validationSchema = yup.object({
-    mediaListNumber: yup
-        .number()
-        .min(1, "Min is 1.")
-        .max(50, "Max is 50.")
-        .required("This field is required.")
-});
+        textAlign: "center"
+    }
+})
 
 export function HomeView() {
-    const [mediaListId, setMediaListId] = useState<number>(2);
-    const [currentPage, setCurrentPage] = useState<number>(1);
-    const [numberOfPages, setNumberOfPages] = useState<number>(1);
-    const [mediaList, setMediaList] = useState<MediaToDisplay[]>([]);
-
-    const history = useHistory();
-
-    useEffect(() => {
-        getMediaList(mediaListId).then();
-    }, [mediaListId]);
-
-    useEffect(() => {
-        getMediaList(mediaListId, currentPage).then();
-    }, [currentPage])
-
-    async function getMediaList(mediaListId: number, page: number = 1): Promise<void> {
-        const list = await MediaRestApi().postMediaList({
-            MediaListId: mediaListId,
-            IncludeCategories: false,
-            IncludeImages: true,
-            IncludeMedia: false,
-            PageNumber: page,
-            PageSize: 15,
-        });
-
-        console.log(list);
-        setMediaList(mediaToDisplay(list.Entities));
-        setNumberOfPages(getNumberOfPages(list.TotalCount));
-    }
-
-    function mediaToDisplay(mediaList: MediaDto[]): MediaToDisplay[] {
-        return mediaList.map(media => {
-            const frameImage: ImageDto | undefined = media.Images.find(image => image.ImageTypeCode === 'FRAME'
-            );
-            const frameImageUrl = frameImage ? frameImage.Url : `${PATH_FOR_IMAGES}/noImageFound.png`;
-            return {
-                id: media.Id,
-                title: media.Title,
-                image: frameImageUrl,
-            };
-        });
-    }
-
-    function getNumberOfPages(totalMediaCount: number): number {
-        return Math.ceil(totalMediaCount / 15);
-    }
-
-    const handlePageChange = (page: number) => {
-        setCurrentPage(page);
-    }
-
-    function playVideo(id: number) {
-        history.push(`/home/${id}`);
-    }
-
-    const formik = useFormik({
-        initialValues: {
-            mediaListNumber: mediaListId - 1
-        },
-        validationSchema: validationSchema,
-        onSubmit: (values) => {
-            setMediaListId(values.mediaListNumber + 1);
-        },
-    });
-
     const classes = useStyles();
     return (
         <div>
-            <form onSubmit={formik.handleSubmit}>
-                <TextField
-                    id="mediaListId"
-                    value={formik.values.mediaListNumber}
-                    type="number"
-                    label="Choose list of media"
-                    name="mediaListNumber"
-                    variant="outlined"
-                    onChange={formik.handleChange}
-                    error={formik.touched.mediaListNumber && Boolean(formik.errors.mediaListNumber)}
-                    helperText={formik.touched.mediaListNumber && formik.errors.mediaListNumber}
-                />
-                <Button type="submit" variant="contained" color="primary">
-                    Load list
-                </Button>
-            </form>
-            <br/>
-            {mediaList.map(media => (<div key={media.id}>
-                    <div>{media.title}</div>
-                    <div className={classes.container}>
-                        <div className={classes.imageWrapper}>
-                            <img className={classes.image} src={media.image} alt=""/>
-                            <IconButton className={classes.playButton} color="default"
-                                        onClick={() => playVideo(media.id)}>
-                                <PlayCircleOutlineIcon className={classes.iconSize}/>
-                            </IconButton>
-                        </div>
-                    </div>
-                    <br/>
-                </div>
-            ))}
-            <div>
-                <Pagination count={numberOfPages} color="primary"
-                            onChange={(event, page) => handlePageChange(page)}/>
-            </div>
+            <VerticalSpace height="3rem"/>
+            <Typography className={classes.container} variant="h6">
+                Let's check our videos!
+            </Typography>
+            <VerticalSpace height="3rem"/>
+            <MediaListView mediaListId={2}/>
+            <VerticalSpace height="3rem"/>
+            <MediaListView mediaListId={3}/>
+            <VerticalSpace height="3rem"/>
+            <MediaListView mediaListId={4}/>
+            <VerticalSpace height="5rem"/>
         </div>
     )
 }
---
+++ src/components/MediaListView/MediaListView.tsx
@@ -0,0 +1,147 @@
+import React, {useEffect, useState} from 'react';
+import {useHistory} from "react-router-dom";
+import {MediaRestApi} from "../../restapi/media/MediaRestApi";
+import {MediaDto} from "../../restapi/media/MediaDto";
+import {ImageDto} from "../../restapi/media/ImageDto";
+import {PATH_FOR_IMAGES} from "../constants/imgPath";
+import {GridList, GridListTile, GridListTileBar, IconButton} from "@material-ui/core";
+import PlayCircleOutlineIcon from "@material-ui/icons/PlayCircleOutline";
+import Pagination from "@material-ui/lab/Pagination";
+import {makeStyles} from "@material-ui/core/styles";
+import {VerticalSpace} from "../VerticalSpace";
+
+type MediaToDisplay = {
+    id: number;
+    title: string;
+    image: string;
+}
+
+const useStyles = makeStyles({
+    root: {
+        display: 'flex',
+        flexWrap: 'wrap',
+        justifyContent: 'space-around',
+        overflow: 'hidden',
+    },
+    gridList: {
+        flexWrap: 'nowrap',
+    },
+    gridListTitle: {
+        maxWidth: "500px",
+        width: "auto !important",
+        height: "100% !important"
+    },
+    imageWrapper: {
+        position: "relative",
+    },
+    image: {
+        objectFit: "cover",
+        width: "100%",
+        height: "100%",
+        "&:hover": {
+            opacity: "70%"
+        }
+    },
+    playButton: {
+        position: "absolute",
+        top: "30%",
+        left: "35%",
+        zIndex: 20,
+        "&:hover": {
+            color: "#ffffff"
+        }
+    },
+    iconSize: {
+        fontSize: "4em"
+    },
+    center: {
+        marginLeft: "50%"
+    }
+});
+
+
+export function MediaListView(props: { mediaListId: number }) {
+    const [currentPage, setCurrentPage] = useState<number>(1);
+    const [numberOfPages, setNumberOfPages] = useState<number>(1);
+    const [mediaList, setMediaList] = useState<MediaToDisplay[]>([]);
+
+    const history = useHistory();
+
+    useEffect(() => {
+        getMediaList(props.mediaListId).then();
+    }, []);
+
+    useEffect(() => {
+        getMediaList(props.mediaListId, currentPage).then();
+    }, [currentPage])
+
+    async function getMediaList(mediaListId: number, page: number = 1): Promise<void> {
+        const list = await MediaRestApi().postMediaList({
+            MediaListId: mediaListId,
+            IncludeCategories: false,
+            IncludeImages: true,
+            IncludeMedia: false,
+            PageNumber: page,
+            PageSize: 15,
+        });
+
+        console.log(list);
+        setMediaList(mediaToDisplay(list.Entities));
+        setNumberOfPages(getNumberOfPages(list.TotalCount));
+    }
+
+    function mediaToDisplay(mediaList: MediaDto[]): MediaToDisplay[] {
+        return mediaList.map(media => {
+            const frameImage: ImageDto | undefined = media.Images.find(image => image.ImageTypeCode === 'FRAME'
+            );
+            const frameImageUrl = frameImage ? frameImage.Url : `${PATH_FOR_IMAGES}/noImageFound.png`;
+            return {
+                id: media.Id,
+                title: media.Title,
+                image: frameImageUrl,
+            };
+        });
+    }
+
+    function getNumberOfPages(totalMediaCount: number): number {
+        return Math.ceil(totalMediaCount / 15);
+    }
+
+    const handlePageChange = (page: number) => {
+        setCurrentPage(page);
+    }
+
+    function playVideo(id: number) {
+        history.push(`/home/${id}`);
+    }
+
+    const classes = useStyles();
+    return (
+        <>
+            <div className={classes.root}>
+                <GridList className={classes.gridList}>
+                    {mediaList.map((media) => (
+                        <GridListTile key={media.id} className={classes.gridListTitle}>
+                            <div className={classes.imageWrapper}>
+                                <img className={classes.image} src={media.image} alt={media.title}/>
+                                        <IconButton className={classes.playButton} color="default"
+                                                    onClick={() => playVideo(media.id)}>
+                                            <PlayCircleOutlineIcon className={classes.iconSize}/>
+                                        </IconButton>
+                            </div>
+                            <GridListTileBar
+                                title={media.title}
+                            />
+                        </GridListTile>
+                    ))}
+                </GridList>
+            </div>
+
+            <div className={classes.center}>
+                <VerticalSpace height="0.5rem"/>
+                <Pagination count={numberOfPages} color="primary"
+                            onChange={(event, page) => handlePageChange(page)}/>
+            </div>
+        </>
+    )
+}
